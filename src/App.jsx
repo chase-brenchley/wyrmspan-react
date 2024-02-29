4045730eedc8cb458420f29d11b7694f12d262aa
Chase Brenchley <chase.brenchley@gmail.com>
2024-02-29 22:55:47 +0000

    Add background and set up glass

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+function App () {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+      {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -27,7 +27,12 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
+      <h1>Welcome to Wyrmspan</h1>
+      <form>
+        <label>Create a game</label>
+        <input type='text' />
+      </form>
     </>
   )
 }
